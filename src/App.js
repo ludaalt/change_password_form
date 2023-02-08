80150c47012185560671d8b6cae839673ed439c4
Людмила Алтухова <luda.tamb@mail.ru>
2023-02-08 12:40:03 +0300

    feat: implement main component

--- src/App.js
+++ src/App.js
@@ -1,2 +1,17 @@
-const App = () => {};
+import styled from "styled-components";
+import Form from "./components/Form";
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const App = () => {
+  return (
+    <AppContainer>
+      <Form />
+    </AppContainer>
+  );
+};
 export default App;
