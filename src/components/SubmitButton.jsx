c5b0d93944693e5f8a29c8ffaf0cffe7e97a1fbc
Людмила Алтухова <luda.tamb@mail.ru>
2023-02-08 11:12:53 +0300

    feat: add submit button component

---
+++ src/components/SubmitButton.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  padding: 20px 80px 20px 40px;
+  margin: 30px 0;
+  background: #ffd970;
+
+  background-image: url("./img/icons/arrow.svg");
+  background-repeat: no-repeat;
+  background-position: 85% 50%;
+
+  border-radius: 5px;
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 17px;
+  line-height: 21px;
+
+  color: #0e406a;
+
+  :hover {
+    background-color: #f3c649;
+    background-position: 90% 50%;
+    transition: background-position 0.3s;
+  }
+`;
+
+const SubmitButton = ({ title }) => {
+  return <StyledButton type="submit">{title}</StyledButton>;
+};
+
+export default SubmitButton;
