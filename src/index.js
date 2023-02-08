ac609b79cb012febf0e566274f22fbdfe79cb201
Людмила Алтухова <luda.tamb@mail.ru>
2023-02-08 12:39:06 +0300

    refactor: apply styled components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {   
+    background: linear-gradient(124.93deg, #F5FCFE 0%, #FFFCF4 99.63%);
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: none;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
